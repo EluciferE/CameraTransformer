d6612b4e198bb5a2d957a3201fb36acd9b02409f
EluciferE <luciferevgeniy@bk.ru>
2021-11-23 17:52:34 +0700

    Work with json, base64. Add gray_button

--- front/javascript/index.js
+++ front/javascript/index.js
@@ -1,24 +1,77 @@
 const PROTOCOL = {
     ready: "r",
-    update_cam : "c",
-    button_pressed : "b",
-    negative_button: "negative",
+    update_cam: "c",
+    negative: "negative",
     mirror_x: "mirror_x",
     mirror_y: "mirror_y",
     blur: "blur",
+    gray: "gray",
+    update_times: "t",
 };
 
 
 let socket;
+let num_frames = 0;
+let output_time = 0;
 setup();
 
 function setup() {
     socket = new WebSocket('ws://localhost:8765');
     socket.addEventListener('message', function (event) {
-        const image = document.getElementById("camera");
-        const blob = new Blob([event.data], {type: "image/jpeg"});
-        image.src = URL.createObjectURL(blob);
-        socket.send(PROTOCOL.update_cam);
+        let json = JSON.parse(event.data);
+
+        const type = json["type"];
+        let data = json["data"];
+
+        switch (type) {
+            case PROTOCOL.update_cam:
+                const t1 = new Date().getTime();
+
+                const image = document.getElementById("camera");
+
+                // Convert base64 to bytes
+                data = atob(data);
+                const len = data.length;
+                const bytes = new Uint8Array(len);
+
+                for (let i = 0; i < len; i++)
+                    bytes[i] = data.charCodeAt(i);
+
+                const blob = new Blob([bytes], {type: "image/jpeg"});
+                image.src = URL.createObjectURL(blob);
+
+                const t2 = new Date().getTime();
+                output_time += (t2 - t1)/1000;
+
+                num_frames += 1
+                if (num_frames % 50 === 0) {
+                    socket.send(PROTOCOL.update_times);
+                }
+
+                socket.send(PROTOCOL.update_cam);
+                break;
+
+            case PROTOCOL.update_times:
+                let total_time = data["total_time"] + output_time
+                document.getElementById("fps").innerText = "FPS: " +
+                    ((num_frames/total_time).toFixed(2)).toString();
+
+                document.getElementById("input_time").innerText = "Input time: " +
+                    (Math.round(data["input_time"]/total_time * 100)).toString() + "%";
+
+                document.getElementById("transform_time").innerText = "Transform time: " +
+                    (Math.round(data["transform_time"]/total_time * 100)).toString() + "%";
+
+                document.getElementById("output_time").innerText = "Output time: " +
+                    (Math.round(output_time/total_time * 100)).toString() + "%";
+
+                console.log("DONE");
+
+                num_frames = 0;
+                output_time = 0;
+
+                break;
+        }
     });
 
     socket.onopen = function () {
@@ -29,15 +82,15 @@ function setup() {
 }
 
 
-function negative_button(){
+function negative_button() {
     if (document.getElementById("negative").style.backgroundColor === "rgb(128, 128, 128)")
         document.getElementById("negative").style.backgroundColor = "rgb(17, 16, 0)";
     else
         document.getElementById("negative").style.backgroundColor = "rgb(128, 128, 128)";
-    socket.send(PROTOCOL.negative_button);
+    socket.send(PROTOCOL.negative);
 }
 
-function mirror_x_button(){
+function mirror_x_button() {
     if (document.getElementById("mirror_x").style.backgroundColor === "rgb(128, 128, 128)")
         document.getElementById("mirror_x").style.backgroundColor = "rgb(17, 16, 0)";
     else
@@ -45,17 +98,26 @@ function mirror_x_button(){
     socket.send(PROTOCOL.mirror_x);
 }
 
-function mirror_y_button(){
+function mirror_y_button() {
     if (document.getElementById("mirror_y").style.backgroundColor === "rgb(128, 128, 128)")
         document.getElementById("mirror_y").style.backgroundColor = "rgb(17, 16, 0)";
     else
         document.getElementById("mirror_y").style.backgroundColor = "rgb(128, 128, 128)";
     socket.send(PROTOCOL.mirror_y);
 }
-function blur_button(){
+
+function blur_button() {
     if (document.getElementById("blur").style.backgroundColor === "rgb(128, 128, 128)")
         document.getElementById("blur").style.backgroundColor = "rgb(17, 16, 0)";
     else
         document.getElementById("blur").style.backgroundColor = "rgb(128, 128, 128)";
     socket.send(PROTOCOL.blur);
+}
+
+function gray_button() {
+    if (document.getElementById("gray").style.backgroundColor === "rgb(128, 128, 128)")
+        document.getElementById("gray").style.backgroundColor = "rgb(17, 16, 0)";
+    else
+        document.getElementById("gray").style.backgroundColor = "rgb(128, 128, 128)";
+    socket.send(PROTOCOL.gray);
 }
