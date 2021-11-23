031b06ca84bbbc3059c787dc2f82fd6836970da3
EluciferE <luciferevgeniy@bk.ru>
2021-11-23 17:51:14 +0700

    update_time + gray

--- back/protocol.py
+++ back/protocol.py
@@ -1,7 +1,9 @@
 class PROTOCOL:
     ready = "r"
     update_cam = "c"
-    negative_button = "negative"
+    update_times = "t"
+    negative = "negative"
     mirror_x = "mirror_x"
     mirror_y = "mirror_y"
     blur = "blur"
+    gray = "gray"
